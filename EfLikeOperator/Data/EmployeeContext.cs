0c7c215222573ef3616c93298f8660574eb3d3d9
David Paquette <dave@davepaquette.com>
2017-10-01 19:43:33 -0600

    Initial commit

---
+++ EfLikeOperator/Data/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfLikeOperator.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        
+        public EmployeeContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EfLikeOperator/Pages/Index.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EfLikeOperator.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfLikeOperator.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly EmployeeContext _context;
+
+        public IndexModel(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Employee> EmployeesLikeTibbs = new List<Employee>();
+        public IEnumerable<Employee> EmployeesContainingTibbs = new List<Employee>();
+
+        public void OnGet()
+        {            
+            EmployeesContainingTibbs = _context.Employees.Where(e => e.LastName.Contains("Tibbs")).ToList();            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//EmployeesLikeTibbs = _context.Employees.Where(e => EF.Functions.Like(e.LastName, "Tibbs")).ToList();
